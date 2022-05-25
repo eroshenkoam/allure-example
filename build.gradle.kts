1e0041e54e94ccf5d6eb47618b47b9345ba2b8c0
Artem Eroshenko <eroshenkoam@qameta.io>
2022-05-25 19:58:44 +0300

    migrate to java17 (via #77)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,29 @@
-import io.qameta.allure.gradle.AllureExtension
-
-buildscript {
-    repositories {
-        maven("https://plugins.gradle.org/m2/")
-    }
-
-    dependencies {
-        classpath("io.qameta.allure:allure-gradle:2.6.0")
-    }
-}
-
-tasks.existing(Wrapper::class) {
-    gradleVersion = "4.10.2"
-    distributionType = Wrapper.DistributionType.ALL
+plugins {
+    java
+    id("io.qameta.allure") version "2.9.4"
 }
 
 group = "io.eroshenkoam"
 version = version
 
-plugins {
-    java
-    maven
-}
-
-apply(plugin = "io.qameta.allure")
-
-configure<AllureExtension> {
-    autoconfigure = true
-    aspectjweaver = true
-    version = "2.14.0"
-
-    useJUnit5 {
-        version = "2.14.0"
+allure {
+    report {
+        version.set("2.18.1")
     }
-
+    adapter {
+        autoconfigure.set(true)
+        aspectjWeaver.set(true)
+        frameworks {
+            junit5 {
+                adapterVersion.set("2.18.1")
+            }
+        }
+    }
 }
 
 tasks.withType(JavaCompile::class) {
-    sourceCompatibility = "${JavaVersion.VERSION_1_8}"
-    targetCompatibility = "${JavaVersion.VERSION_1_8}"
+    sourceCompatibility = "${JavaVersion.VERSION_17}"
+    targetCompatibility = "${JavaVersion.VERSION_17}"
     options.encoding = "UTF-8"
 }
 
@@ -60,9 +45,9 @@ repositories {
 }
 
 dependencies {
-    compile("commons-io:commons-io:2.6")
-    compile("io.qameta.allure:allure-java-commons:2.14.0")
-    compile("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    compile("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-    compile("org.junit.jupiter:junit-jupiter-params:5.7.2")
+    implementation("commons-io:commons-io:2.6")
+    implementation("io.qameta.allure:allure-java-commons:2.14.0")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    implementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
 }
