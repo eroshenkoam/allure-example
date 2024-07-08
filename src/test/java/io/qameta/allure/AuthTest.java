a34dca6e921e6b599ba07ce2e7c8f75a3d143002
Artem Eroshenko <artem.eroshenko@qameta.io>
2024-07-08 15:58:06 +0300

    add new tet 5283

--- src/test/java/io/qameta/allure/AuthTest.java
+++ src/test/java/io/qameta/allure/AuthTest.java
@@ -11,9 +11,33 @@ import static io.qameta.allure.Allure.step;
 public class AuthTest {
 
     @Test
+    @AllureId("5283")
+    @DisplayName("Авторизация через Яндекс")
+    public void testYandexAuth() {
+        step("Открываем главную страницу");
+        step("Авторизуемся через Яндекс", () -> {
+            step("Нажиманем кнопку Войти");
+            step("Выбираем авторизацию Яндекс");
+            step("Вводим логин eroshenkoam@yandex.ru");
+            step("Вводим пароль 123ш812398123");
+            step("Нажимаем кнопку Войти");
+        });
+        step("Проверяем что авторизовались правильно");
+        step("Проверяем что данные пользователя синхронизировались из Яндекс", () -> {
+            step("Открываем профиль пользователя");
+            step("Видим информацию как на скриншоте");
+            step("Expected Result", () -> {
+                step("Проверяем что Имя пользователь = Артем Ерошенко");
+                step("Email = eroshenkoam@yandex.ru");
+                step("Login = eroshenkoam");
+            });
+        });
+    }
+
+    @Test
     @AllureId("5284")
     @DisplayName("Деавторизция через Яндекс")
-    public void testYandex() {
+    public void testYandexDeauth() {
         step("Открываем главную страницу");
         step("Авторизуемся через Яндекс", () -> {
             step("Нажиманем кнопку Войти");
