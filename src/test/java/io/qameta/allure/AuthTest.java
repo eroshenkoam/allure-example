e1fc87b250842b2157a682668224f1c6439f1a69
Artem Eroshenko <artem.eroshenko@qameta.io>
2024-08-28 13:09:42 +0300

    add new test #6635

--- src/test/java/io/qameta/allure/AuthTest.java
+++ src/test/java/io/qameta/allure/AuthTest.java
@@ -33,4 +33,33 @@ public class AuthTest {
         });
     }
 
+    @Test
+    @AllureId("6635")
+    @DisplayName("Авторизация через Google")
+    @Tag("web")
+    @Story("External Auth")
+    @Owner("artem.eroshenko@qameta.io")
+    @Feature("Auth")
+    public void testGoogle() {
+        step("Открываем главную страницу");
+        step("Авторизация через Яндекс", () -> {
+            step("Нажимаем кнопку Log In");
+            step("Выбираем способ авторизации через Яндекс");
+            step("Вводим логин eroshenkoam@yandex.ru");
+            step("Вводим пароль 1239812983123");
+            step("Нажимаем кнопку Войти");
+        });
+        step("Проверяем что авторизовались", () -> {
+            step("Expected Result", () -> {
+                step("В левом нижнем углу видим иконку пользотваеля");
+            });
+        });
+        step("Проверяем что данные пользователя обновились из Яндекса", () -> {
+            step("Expected Result", () -> {
+                step("Email = eroshenkoam@yandex.ru");
+                step("Name = Artem Eroshenko");
+            });
+        });
+    }
+
 }
