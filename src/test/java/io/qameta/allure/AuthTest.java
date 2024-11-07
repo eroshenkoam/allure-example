d40c599df9f277921613ca2a5dbd7a846ef91228
Artem Eroshenko <eroshenkoam@gmail.com>
2024-11-07 15:56:39 +0300

    add new test 10799

---
+++ src/test/java/io/qameta/allure/AuthTest.java
@@ -0,0 +1,32 @@
+package io.qameta.allure;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@Owner("eroshenkoam")
+@Feature("Auth")
+@Story("External Auth")
+public class AuthTest {
+
+    @Test
+    @AllureId("10799")
+    @DisplayName("Авторизация через Одноклассники")
+    public void testOdnoklassniki() {
+        step("Открываем главную страницу");
+        step("Авторизуемся через Яндекс", () -> {
+            step("Вводим логин ranadom-user@yandex.ru");
+            step("Вводим пароль random-pass");
+            step("Нажимаем кнопку Войти");
+        });
+        step("Проверяем что авторизовались");
+        step("Проверяем что данные пользователя обновиилсь из Яндекс", () -> {
+            step("Expected Result", () -> {
+                step("Имя = Mr. Random");
+                step("Email = random-user@yandex.ru");
+            });
+        });
+    }
+
+}
